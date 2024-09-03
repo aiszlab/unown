18c979c77a2af53a09573a00972be14fea68c781
murukal <tutu@fantufantu.com>
2024-09-03 09:16:00 +0800

    feat: use bee & wasp

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import logo from './logo.svg'
 import './App.css'
-import { StrictMode } from 'react'
 
 function App() {
   return (
-    <StrictMode>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
-      </div>
-    </StrictMode>
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
   )
 }
 
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import App from './App'
+
+import { bootstrap } from '@aiszlab/bee'
+
+bootstrap({
+  selectors: '#root',
+  render: App
+})
