cd3cc8177ebdc16962b2d41912d6e963d2ab54d3
murukal <tutu@fantufantu.com>
2024-09-04 09:30:45 +0800

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <p>
-          Edit <code className="bg-red">src/App.tsx</code> and save to reload.
+          Edit <code className='bg-red'>src/App.tsx</code> and save to reload.
         </p>
         <a className='App-link mt-5' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
